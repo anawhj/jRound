53136aef2fe550a5efa63bb6336a104dceba9376
Jihye Hong <jh.hong@lge.com>
2015-09-23 17:10:58 +0900

    Remove space error

--- src/css-polar.js
+++ src/css-polar.js
@@ -13,16 +13,16 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License. */
- 
+
 (function(w) {
     "use strict";
     var jRound = {};
     var drawPolar = function(polar_id) {
         var polarElement = document.querySelector(polar_id);
-        
+
         var containingBlock = polarElement.parentNode;
 		var containingBlockWidth = parseFloat(window.getComputedStyle(containingBlock, null).getPropertyValue("width"));
-		
+
 		if (typeof polarElement.tagName !== 'undefined') {
 			var polarElementWidth = parseFloat(window.getComputedStyle(polarElement, null).getPropertyValue("width"));
 			if (polarElement.dataset.polarDistance !== undefined) {
@@ -35,33 +35,33 @@
 				var polarAnchor = polarElement.dataset.polarAnchor;
 				if (polarAnchor == undefined)
 					polarAnchor = "center";
-					
+
 				console.log("distance: " + polarDistance);
 				console.log("angle: " + polarAngle);
 				console.log("anchor: " + polarAnchor);
-				
+
 				polarAngle = parseFloat(polarAngle);
-				
+
 				if (polarDistance.indexOf("%") > 0) {
 					polarDistance = (containingBlockWidth / 2) * (parseFloat(polarDistance) / 100.0);
 				} else { // if (polarDistance.indexOf("px") > 0) {
 					polarDistance = parseFloat(polarDistance);
 				}
-						
-				var anchorPoint = getAnchorPoint(polarAnchor, containingBlockWidth, polarElementWidth, polarAngle, polarDistance);			
-				
+
+				var anchorPoint = getAnchorPoint(polarAnchor, containingBlockWidth, polarElementWidth, polarAngle, polarDistance);
+
 				console.log("x: " + anchorPoint.x + " y: " + anchorPoint.y);
-				
+
 				var translate3d = "translate3d(" + anchorPoint.x + "px, " + anchorPoint.y + "px, 0px)";
-				
+
 				polarElement.style.transform = translate3d;
 			}
 		}
-        
+
     },
 	getAnchorPoint = function(valueString, containingBlockWidth, elementWidth, polarAngle, polarDistance) {
 		var valueList = valueString.split(' ');
-				
+
 		var anchorX, anchorY;
         switch (valueList.length) {
             case 0:
@@ -84,7 +84,7 @@
 				} else if (valueList[0] == "right") {
 					anchorX = elementWidth;
 					anchorY = elementWidth/2;
-				}				
+				}
                 break;
             case 2:
                 if (valueList[0] == "center") {
@@ -100,7 +100,7 @@
 						anchorX = parseFloat(valueList[0]);
 					}
 				}
-				
+
 				if (valueList[1] == "center") {
 					anchorY = elementWidth/2;
 				} else if (valueList[1] == "top") {
@@ -120,26 +120,26 @@
 					anchorX = elementWidth/2;
 				} else if (valueList[0] == "left") {
 					anchorX = 0;
-					
+
 					if (valueList[1].indexOf("%") > 0) {
 						anchorX = anchorX +  elementWidth * (parseFloat(valueList[1]) / 100.0);
 					} else if (valueList[1].indexOf("px") > 0) {
 						anchorX = anchorX + parseFloat(valueList[1]);
 					} else {
-						
+
 					}
 				} else if (valueList[0] == "right") {
 					anchorX = elementWidth;
-					
+
 					if (valueList[1].indexOf("%") > 0) {
 						anchorX = anchorX +  elementWidth * (1 - (parseFloat(valueList[1]) / 100.0));
 					} else if (valueList[1].indexOf("px") > 0) {
 						anchorX = anchorX - parseFloat(valueList[1]);
 					} else {
-						
+
 					}
 				}
-				
+
                 break;
 			case 4:
 				//for the first value
@@ -147,7 +147,7 @@
 					anchorX = elementWidth/2;
 				} else if (valueList[0] == "left") {
 					anchorX = 0;
-					
+
 					if (valueList[1].indexOf("%") > 0) {
 						anchorX = anchorX +  elementWidth * (parseFloat(valueList[1]) / 100.0);
 					} else if (valueList[1].indexOf("px") > 0) {
@@ -155,20 +155,20 @@
 					}
 				} else if (valueList[0] == "right") {
 					anchorX = elementWidth;
-					
+
 					if (valueList[1].indexOf("%") > 0) {
 						anchorX = anchorX +  elementWidth * (1 - (parseFloat(valueList[1]) / 100.0));
 					} else if (valueList[1].indexOf("px") > 0) {
 						anchorX = anchorX - parseFloat(valueList[1]);
 					}
 				}
-				
+
 				//for the second value
 				if (valueList[2] == "center") {
 					anchorY = elementWidth/2;
 				} else if (valueList[2] == "top") {
 					anchorY = 0;
-					
+
 					if (valueList[3].indexOf("%") > 0) {
 						anchorY = anchorY +  elementWidth * (parseFloat(valueList[3]) / 100.0);
 					} else if (valueList[3].indexOf("px") > 0) {
@@ -176,7 +176,7 @@
 					}
 				} else if (valueList[2] == "bottom") {
 					anchorY = elementWidth;
-					
+
 					if (valueList[3].indexOf("%") > 0) {
 						anchorY = anchorY +  elementWidth * (1 - (parseFloat(valueList[3]) / 100.0));
 					} else if (valueList[3].indexOf("px") > 0) {
@@ -185,12 +185,12 @@
 				}
 				break;
         }
-		
+
 		var point = {
 			x: Math.sin(Math.PI / 180 * polarAngle) * polarDistance + (containingBlockWidth / 2) - anchorX,
 			y: -Math.cos(Math.PI / 180 * polarAngle) * polarDistance + (containingBlockWidth / 2) - anchorY
 		};
-		
+
 		return point;
     },
     init = function() {
@@ -215,7 +215,7 @@
             if (document.querySelector(idList[i].selector)) {
                 drawPolar(idList[i].selector);
             }
-        }		
+        }
     };
     window.addEventListener("load", function() {
         if (typeof w.jRound === "undefined") {
